ca9ffb6d6ae38285d1e309ff423b84b0afbcbd81
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-10-09 02:01:24 +0100

    fix: add main app database connection here to prevent clash of databases with test database

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const app = require("./app");
+const mongoose = require("mongoose");
 
 // configure app port
 const { API_PORT } = process.env;
 const PORT = process.env.PORT || API_PORT;
+
+// connect to mongodb
+const dbURI = process.env.dbURI;
+mongoose.connect(dbURI, (error) => {
+  if (!error) {
+    console.log("db connected successfully!");
+  } else {
+    console.log("An error occured: ", error);
+  }
+})
 
 app.listen(PORT, () => {
     console.log("server started successfully!")
