11538662cb8320a69eeec45a91c96d4c89074b7d
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-10-14 03:07:19 +0100

    test: add tests for Authentication login

--- test/integration/auth.routes.test.js
+++ test/integration/auth.routes.test.js
@@ -41,7 +41,7 @@ describe("Authentication Register", () => {
         expect(response.headers['content-type']).toContain("text/html")
     })
 
-    it("should fail to POST /auth/register due to invalid email", async () => {
+    it("should fail to POST /auth/register due to invalid email address", async () => {
         const userAuthData = {
             username: "testuser",
             email: "test?",
@@ -104,5 +104,70 @@ describe("Authentication Register", () => {
         const response = await supertest(app).post("/auth/register").send(userAuthData);
         expect(response.statusCode).toBe(400);
         expect(response.headers['content-type']).toContain("text/html")
+    })
+})
+
+describe("Authentication Login", () => {
+    it("should GET /auth/login", async () => {
+        const response = await supertest(app).get("/auth/login");
+        expect(response.statusCode).toBe(200);
+        expect(response.headers['content-type']).toContain("text/html")
+    })
+
+    it("should fail to POST /auth/login due to invalid email address", async () => {
+        const userAuthData = {
+            email: "test?",
+            password: "testuser",
+        }
+
+        const response = await supertest(app).post("/auth/login").send(userAuthData);
+        expect(response.statusCode).toBe(400);
+        expect(response.headers['content-type']).toContain("text/html")
+    })
+
+    it("should fail to POST /auth/login due to short password", async () => {
+        const userAuthData = {
+            email: "testuser@test.com",
+            password: ""
+        }
+
+        const response = await supertest(app).post("/auth/login").send(userAuthData);
+        expect(response.statusCode).toBe(400);
+        expect(response.headers['content-type']).toContain("text/html")
+    })
+
+    it("should fail to POST /auth/login due to invalid password", async () => {
+        const userAuthData = {
+            email: "testuser@test.com",
+            password: "test"
+        }
+
+        const response = await supertest(app).post("/auth/login").send(userAuthData);
+        expect(response.statusCode).toBe(401);
+        expect(response.headers['content-type']).toContain("text/html")
+    })
+
+    it("should fail to POST /auth/login due to unexisting user", async () => {
+        const userAuthData = {
+            email: "idont@exist.com",
+            password: "testnouser",
+        }
+
+        const response = await supertest(app).post("/auth/login").send(userAuthData);
+        expect(response.statusCode).toBe(400);
+        expect(response.headers['content-type']).toContain("text/html")
+    })
+
+    it("should POST /auth/login successfully ", async () => {
+        const userAuthData = {
+            username: "testuser",
+            email: "testuser@test.com",
+            password: "testuser",
+            confirmPassword: "testuser"
+        }
+
+        const response = await supertest(app).post("/auth/login").send(userAuthData);
+        expect(response.statusCode).toBe(302);
+        expect(response.headers['content-type']).toContain("text/plain")
     })
 })
